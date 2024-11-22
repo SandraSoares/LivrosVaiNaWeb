567e7fed9ae5bdcbeb8cc918cbcf4fff4f442d8b
SandraSoares <sandradcsoares@gmail.com>
2024-11-22 19:03:58 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from './Components/Header/Header'
+import './globalStyle/globalStyle.scss'
+export default function App(){
+  return (
+    <section>
+      <Header/>
+    </section>
+  )
+}
